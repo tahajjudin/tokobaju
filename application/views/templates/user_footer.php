f9a891e69288ef12a14c72f05282e8e120568be5
E41180658muhammadjamalludin <mochjamalludin@gmail.com>
2021-09-12 23:39:21 +0700

    user home
    user home dengan backend

---
+++ application/views/templates/user_footer.php
@@ -0,0 +1,22 @@
+    <footer>
+        <ul class="info">
+            <li>more info</li>
+            <li><a href="">search</a></li>
+            <li><a href="">faq</a></li>
+            <li><a href="">returm & exchange</a></li>
+            <li><a href="">policy</a></li>
+            <li><a href="">contact</a></li>
+            <li><a href="">privacy policy</a></li>
+        </ul>
+        <ul class="info">
+            <li>social media</li>
+            <li><a href="">facebook</a></li>
+            <li><a href="">instagram</a></li>
+            <li><a href="">whatsapp</a></li>
+            <li><a href="">youtube</a></li>
+        </ul>
+        <ul class="info">
+            <li><img src="<?= base_url('assets/img/mascitra merc.png')?>" alt=""></li>
+        </ul>
+        <div class="copyright"><a href="">© 2021, Mascitra.Merch</a></div>
+    </footer>
